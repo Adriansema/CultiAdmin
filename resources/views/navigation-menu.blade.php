8cd07b69e51e7e4dcddaf8eba355f929729a7113
Adriansema <adrianjandro1@gmail.com>
2025-04-10 16:30:26 -0500

    imagen de fondo

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -4,7 +4,7 @@
     <!-- Primary Navigation Menu -->
 
     <div class="bg-blue-500  p-2" >
-        <img src="https://zajuna.sena.edu.co/img/logos/gov-logo.svg" alt="" width="500px">
+        <img src="https://zajuna.sena.edu.co/img/logos/gov-logo.svg" alt="" width="100px">
     </div>
     <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
