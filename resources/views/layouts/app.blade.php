d6a82806eb50021dc04da203ed980f5dee155177
RCandres19 <andresconde45678@gmail.com>
2025-04-22 17:16:25 -0500

    actualizando la barra lateral version 1.2 220425

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,6 +32,30 @@
 >>>>>>> Stashed changes
 >>>>>>> 5df863f (actualizando el dashboard)
         <img src="https://zajuna.sena.edu.co/img/logos/gov-logo.svg" alt="" width="100px">
+=======
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    <!-- Styles -->
+    @livewireStyles
+</head>
+<body class="flex flex-col h-screen overflow-hidden font-sans antialiased">
+
+    <!-- Cinta GOV.CO -->
+    <div class="p-1 bg-blue-500">
+        <img src="https://zajuna.sena.edu.co/img/logos/gov-logo.svg" alt="Logo GOV.CO" width="100px">
+>>>>>>> Stashed changes
     </div>
     <x-banner />
 
