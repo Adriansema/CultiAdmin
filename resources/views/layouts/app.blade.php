30ed39e4809303d3bbbe3d742d0f1657963f7c9e
RCandres19 <andresconde45678@gmail.com>
2025-04-23 08:52:45 -0500

    corrigiendo errores en el app y navigation-menu.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,6 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-<<<<<<< HEAD
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
@@ -19,43 +18,12 @@
     @livewireStyles
 </head>
 
-<body class="flex flex-col h-screen overflow-hidden font-sans antialiased">
-    <div class="p-1 bg-blue-500">
-=======
-        <!-- Styles -->
-        @livewireStyles
-    </head>
-<<<<<<< Updated upstream
-    <div class="p-1 bg-blue-500" >
-=======
-    <div class="p-2 bg-blue-500" >
->>>>>>> Stashed changes
->>>>>>> 5df863f (actualizando el dashboard)
-        <img src="https://zajuna.sena.edu.co/img/logos/gov-logo.svg" alt="" width="100px">
-=======
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
-    <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-
-    <!-- Styles -->
-    @livewireStyles
-</head>
 <body class="flex flex-col h-screen overflow-hidden font-sans antialiased">
 
     <!-- Cinta GOV.CO -->
     <div class="p-1 bg-blue-500">
         <img src="https://zajuna.sena.edu.co/img/logos/gov-logo.svg" alt="Logo GOV.CO" width="100px">
->>>>>>> Stashed changes
+
     </div>
     <x-banner />
 
