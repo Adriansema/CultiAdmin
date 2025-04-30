90356d2f3fb0b4000a2bd3221406b5e8f87d2d0c
RCandres19 <andresconde45678@gmail.com>
2025-04-30 09:10:55 -0500

    ajustando los botones del sideba(Barra Lateral), con hover y ademas hice layer base en app.css para el hover de los botones

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -20,9 +20,9 @@
                     <div class="space-y-1">
                         <!-- Botón con borde superior y laterales -->
                         <button type="button"
-                            class="flex items-center w-full px-3 py-2 font-medium transition bg-green-600 border-t-2 rounded-t-md border-x-2 hover:bg-green-800"
+                            class="flex items-center w-full px-4 py-2 font-medium transition bg-green-600 border-t-2 rounded-full border-x-2 hover:bg-green-800"
                             style="border-color: #39A900;" data-toggle="submenu" data-target="#submenu-nueva-entrada">
-                            <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" stroke-width="2"
+                            <svg class="w-5 h-4 mr-2" fill="none" stroke="currentColor" stroke-width="2"
                                 viewBox="0 0 24 24">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M12 4v16m8-8H4" />
                             </svg>
@@ -34,14 +34,14 @@
 
                         <!-- Submenú con borde inferior y laterales -->
                         <div id="submenu-nueva-entrada"
-                            class="hidden pl-6 mt-0 space-y-1 border-b-2 border-x-2 rounded-b-md"
+                            class="hidden pl-0 mt-1 space-y-0 border-b-2 rounded-b-3xl border-x-2"
                             style="border-color: #228B22;">
                             <a href="/nueva-entrada/cultivo"
-                                class="block px-3 py-2 text-sm text-white bg-green-600 rounded-none hover:bg-green-500">
+                                class="block px-3 py-2 text-sm text-center text-white rounded-full hover:bg-green-500">
                                 {{ __('Cultivos') }}
                             </a>
                             <a href="/nueva-entrada/boletin"
-                                class="block px-3 py-2 text-sm text-white bg-green-600 rounded-none hover:bg-green-500">
+                                class="block px-3 py-2 text-sm text-center text-white rounded-full hover:bg-green-500">
                                 {{ __('Boletines') }}
                             </a>
                         </div>
@@ -54,18 +54,35 @@
                 <div class="flex-1 space-y-3">
                     <div class="space-y-3">
                         <!-- Panel -->
-                        <x-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')"
-                            class="flex items-center px-3 py-2 transition rounded-md hover:bg-[#547488]">
-                            <svg class="w-5 h-5 mr-2 text-green-50" fill="none" stroke="currentColor" stroke-width="2"
+                        <x-responsive-nav-link
+                            href="{{ route('dashboard') }}"
+                            :active="request()->routeIs('dashboard')"
+                            class="flex items-center px-3 py-2 transition rounded-xl hover:bg-[var(--color-sidebarhoverbtn)] group">
+
+                            <!-- Ícono de casa -->
+                            <svg class="w-5 h-5 mr-2 transition
+                                {{ request()->routeIs('dashboard')
+                                    ? 'text-gray-700'
+                                    : 'text-green-50 group-hover:text-gray-600' }}"
+                                fill="none" stroke="currentColor" stroke-width="2"
                                 viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                 <path stroke-linecap="round" stroke-linejoin="round"
                                     d="M3 12l9-9 9 9M4 10v10a1 1 0 001 1h5m4 0h5a1 1 0 001-1V10" />
                             </svg>
-                            <span class="text-sm text-white">{{ __('Inicio') }}</span>
+
+                            <!-- Texto -->
+                            <span class="text-sm transition
+                                {{ request()->routeIs('dashboard')
+                                    ? 'text-gray-700'
+                                    : 'text-white group-hover:text-gray-600' }}">
+                                {{ __('Inicio') }}
+                            </span>
                         </x-responsive-nav-link>
 
+
+
                         <button type="button"
-                            class="flex items-center w-full px-3 py-2 font-medium transition rounded-md hover:bg-[#547488]"
+                            class="flex items-center w-full px-3 py-2 font-medium transition rounded-xl hover:bg-[var(--color-sidebarhoverbtn)]"
                             data-toggle="submenu" data-target="#submenu-productos">
                             <svg class="w-5 h-5 mr-2 transition-colors text-green-50 group-hover:text-green-900"
                                 fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24"
@@ -82,7 +99,7 @@
 
                         <div id="submenu-productos" class="hidden mt-1 ml-6 space-y-1">
                             <x-responsive-nav-link href="{{ route('productos.cafe') }}"
-                                class="flex items-center px-3 py-2 text-sm text-gray-600 rounded-md border-2 border-transparent hover:border-[#39A900] hover:text-[#39A900] transition-all duration-300">
+                                class="flex items-center px-3 py-2 text-sm text-gray-600 rounded-xl border-2 border-transparent hover:border-[#39A900] hover:text-[#39A900] transition-all duration-300">
 
                                 <!-- Icono de café -->
                                 <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 mr-2" fill="none"
@@ -95,7 +112,7 @@
                             </x-responsive-nav-link>
 
                             <x-responsive-nav-link href="{{ route('productos.mora') }}"
-                                class="flex items-center px-3 py-2 text-sm text-gray-600 rounded-md border-2 border-transparent hover:border-[#39A900] hover:text-[#39A900] transition-all duration-100">
+                                class="flex items-center px-3 py-2 text-sm text-gray-600 rounded-xl border-2 border-transparent hover:border-[#39A900] hover:text-[#39A900] transition-all duration-100">
 
                                 <!-- Icono de mora (usaré uno genérico de fruta por ahora) -->
                                 <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 mr-2" fill="none"
@@ -113,7 +130,7 @@
                     <!-- Boletines con submenú -->
                     <div class="space-y-1">
                         <button type="button"
-                            class="flex items-center w-full px-3 py-2 font-medium transition rounded-md hover:bg-[#547488]"
+                            class="flex items-center w-full px-3 py-2 font-medium transition rounded-xl hover:bg-[var(--color-sidebarhoverbtn)]"
                             data-toggle="submenu" data-target="#submenu-boletines">
                             <svg class="w-5 h-5 mr-2 text-blue-50" fill="none" stroke="currentColor" stroke-width="2"
                                 viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
@@ -129,7 +146,7 @@
 
                         <div id="submenu-boletines" class="hidden mt-1 ml-6 space-y-1">
                             <x-responsive-nav-link href="{{ route('productos.cafe') }}"
-                                class="flex items-center px-3 py-2 text-sm text-gray-600 rounded-md border-2 border-transparent hover:border-[#39A900] hover:text-[#39A900] transition-all duration-300">
+                                class="flex items-center px-3 py-2 text-sm text-gray-600 rounded-xl border-2 border-transparent hover:border-[#39A900] hover:text-[#39A900] transition-all duration-300">
 
                                 <!-- Icono de café -->
                                 <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 mr-2" fill="none"
@@ -142,7 +159,7 @@
                             </x-responsive-nav-link>
 
                             <x-responsive-nav-link href="{{ route('productos.mora') }}"
-                                class="flex items-center px-3 py-2 text-sm text-gray-600 rounded-md border-2 border-transparent hover:border-[#39A900] hover:text-[#39A900] transition-all duration-300">
+                                class="flex items-center px-3 py-2 text-sm text-gray-600 rounded-xl border-2 border-transparent hover:border-[#39A900] hover:text-[#39A900] transition-all duration-300">
 
                                 <!-- Icono de mora (usaré uno genérico de fruta por ahora) -->
                                 <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 mr-2" fill="none"
@@ -168,18 +185,24 @@
                 <div class="flex-1 space-y-2.5">
                     <x-responsive-nav-link href="{{ route('usuarios.index') }}"
                         :active="request()->routeIs('usuarios.*')"
-                        class="block px-3 py-2 text-sm text-gray-600 rounded-md hover:bg-[#547488]">
+                        class="block px-3 py-2 text-sm text-gray-600 rounded-xl hover:bg-[var(--color-sidebarhoverbtn)] group">
 
-                        <span class="relative inline-flex items-center">
-                            <!-- Ícono "+" ajustado al borde izquierdo -->
-                            <svg class="absolute w-2.5 h-3 translate-x-1 text-purple-50 -left-3" fill="none"
-                                stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">
+                        <span class="relative inline-flex items-center mr-2">
+                            <!-- Ícono "+" -->
+                            <svg class="absolute w-2.5 h-3 translate-x-1 -left-3 transition
+                                {{ request()->routeIs('usuarios.*')
+                                    ? 'text-gray-700'
+                                    : 'text-purple-50 group-hover:text-gray-700' }}"
+                                fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M12 4v16m8-8H4" />
                             </svg>
 
                             <!-- Ícono de dos usuarios -->
-                            <svg class="w-4 h-4 text-purple-50" fill="none" stroke="currentColor" stroke-width="2"
-                                viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <svg class="w-3.5 h-3 transition
+                                {{ request()->routeIs('usuarios.*')
+                                    ? 'text-gray-700'
+                                    : 'text-purple-50 group-hover:text-gray-700' }}"
+                                fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                 <path stroke-linecap="round" stroke-linejoin="round"
                                     d="M17 20v-2a4 4 0 00-4-4H7a4 4 0 00-4 4v2" />
                                 <circle cx="9" cy="7" r="4" stroke-linecap="round" stroke-linejoin="round" />
@@ -187,13 +210,21 @@
                                     d="M23 20v-2a4 4 0 00-3-3.87M16 3.13a4 4 0 010 7.75" />
                             </svg>
                         </span>
-                        <span class="text-sm text-white">{{ __('Gestión de Usuarios') }}</span>
+
+                        <!-- Texto -->
+                        <span class="text-sm transition
+                            {{ request()->routeIs('usuarios.*')
+                                ? 'text-gray-700'
+                                : 'text-white group-hover:text-gray-700' }}">
+                            {{ __('Gestión de Usuarios') }}
+                        </span>
                     </x-responsive-nav-link>
+
 
                     <!-- Botón de Accesibilidad -->
                     <x-responsive-nav-link href="{{ route('accesibilidad.index') }}"
                         :active="request()->routeIs('accesibilidad')"
-                        class="block px-3 py-2 text-sm text-gray-600 rounded-md hover:bg-[#547488]">
+                        class="block px-3 py-2 text-sm text-gray-600 rounded-xl hover:bg-[var(--color-sidebarhoverbtn)]">
                         <!-- Ícono de accesibilidad (persona con brazos extendidos) -->
                         <svg class="inline w-5 h-5 mr-2 text-green-50" fill="none" stroke="currentColor"
                             stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" role="img"
@@ -207,7 +238,7 @@
 
                     <!-- Boton de Centro de Ayuda -->
                     <x-responsive-nav-link href="{{ route('centroAyuda.index') }}"
-                        class="block px-3 py-2 text-sm text-gray-600 rounded-md hover:bg-[#547488]">
+                        class="block px-3 py-2 text-sm text-gray-600 rounded-xl hover:bg-[var(--color-sidebarhoverbtn)]">
                         <svg class="inline w-5 h-5 mr-2 text-blue-50" fill="none" stroke="currentColor" stroke-width="2"
                             viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" role="img"
                             aria-label="Centro de Ayuda">
@@ -222,7 +253,7 @@
                         @csrf
                         <x-responsive-nav-link href="{{ route('logout') }}"
                             onclick="event.preventDefault(); this.closest('form').submit();"
-                            class="block px-3 py-2 text-sm text-gray-800 rounded-md hover:bg-[#547488]">
+                            class="block px-3 py-2 text-sm text-gray-800 rounded-xl hover:bg-[var(--color-sidebarhoverbtn)]">
                             <svg class="inline w-5 h-5 mr-2 text-red-50" fill="none" stroke="currentColor"
                                 stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                 <!-- Palito separado, ajustado para bajar un poco más -->
@@ -239,11 +270,11 @@
             {{-- Foto de perfil y nombre --}}
             <div class="py-2">
                 <x-responsive-nav-link href="{{ route('profile.show') }}"
-                    class="block px-3 py-2 text-sm text-gray-500 hover:bg-[#224F69]">
+                    class="block px-3 py-2 text-sm text-gray-500 hover:bg-[var(--color-sidebarhoverbtn)] rounded-xl">
                     <div
                         class="flex items-center w-full px-3 py-1 space-x-4 bg-gray-100 rounded-2xl min-w-[193px] max-w-sm">
                         {{-- Imagen de perfil --}}
-                        <img class="object-cover rounded-full size-10" src="{{ Auth::user()->profile_photo_url }}"
+                        <img class="object-cover rounded-lg size-10" src="{{ Auth::user()->profile_photo_url }}"
                             alt="{{ Auth::user()->name }}" />
 
                         {{-- Nombre y rol apilados verticalmente --}}
