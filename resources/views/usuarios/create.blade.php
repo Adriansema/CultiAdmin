12332851cf364fd1e8c28e3f10016be69e1c5334
Adriansema <adrianjandro1@gamil.com>
2025-05-26 08:21:29 -0500

    actualizacion del formulario en la parte de colocar o importar nuevos uuarios

--- resources/views/usuarios/create.blade.php
+++ resources/views/usuarios/create.blade.php
@@ -6,26 +6,43 @@
 
 @section('content')
 <div class="max-w-4xl py-6 mx-auto">
-    <hr class="my-6 border-gray-300">
+    <div class="p-6 space-y-6 bg-gray-100 shadow-md rounded-xl">
 
-    <h3 class="text-lg font-semibold text-gray-700">Importar usuarios desde CSV</h3>
-    <form action="{{ route('usuarios.importarCsv') }}" method="POST" enctype="multipart/form-data">
-        @csrf
+        {{-- Encabezado --}}
+        <h3 class="text-lg font-semibold text-gray-700">📥 Importar usuarios desde CSV</h3>
 
-        <x-label for="archivo" value="Importar archivo CSV" />
-        <x-input id="archivo" class="block w-full mt-1" type="file" name="archivo" required />
+        {{-- Formulario de importación CSV --}}
+        <form action="{{ route('usuarios.importarCsv') }}" method="POST" enctype="multipart/form-data" class="space-y-4">
+            @csrf
+            <div class="flex flex-col gap-4 sm:flex-row sm:items-end">
+                <div class="flex-1">
+                    <x-label for="archivo" value="Seleccionar archivo CSV" />
+                    <x-input id="archivo" class="block w-full mt-1" type="file" name="archivo" required />
+                </div>
+                <div>
+                    <x-button class="w-full sm:w-auto">Importar Usuarios</x-button>
+                </div>
+            </div>
+        </form>
 
-        <x-button class="mt-4">
-            Importar Usuarios
-        </x-button>
-    </form>
-    <form action="{{ route('usuarios.store') }}" method="POST">
-        @include('usuarios._form', ['roles' => $roles])
-        <x-button class="mt-4">Guardar</x-button>
-    </form>
-    <a href="{{ route('usuarios.index') }}"
-        class="inline-block px-4 py-2 text-white bg-gray-600 rounded hover:bg-gray-700">
-        ← Volver a la lista
-    </a>
+        {{-- Separador visual opcional --}}
+        <hr class="border-t border-gray-300">
+
+        {{-- Formulario de creación manual --}}
+        <form action="{{ route('usuarios.store') }}" method="POST" class="space-y-6">
+            @csrf
+            @include('usuarios._form', ['roles' => $roles])
+
+            <div class="flex flex-col gap-4 sm:flex-row sm:justify-between sm:items-center">
+                <a href="{{ route('usuarios.index') }}"
+                   class="px-4 py-2 text-center text-white bg-gray-600 rounded hover:bg-gray-700">
+                   ← Volver a la lista
+                </a>
+
+                <x-button class="text-center">Guardar</x-button>
+            </div>
+        </form>
+
+    </div>
 </div>
 @endsection
