eb1a612defc2ffd6a75fe4f8494a73363c5deb94
Adriansema <adrianjandro1@gamil.com>
2025-06-04 17:00:40 -0500

    arreglos de app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,3 +4,9 @@ import './dashboard'; //  Esto ya debe incluir tu gráfica de ECharts
 // Tu nuevo archivo con la gráfica ECharts
 import './modal';
 import './UserSearch'; // Primero, el que maneja la carga y el DOM
+//importando Flatpickr
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css"; // Importa también los estilos CSS de flatpickr
+// --- Importar y configurar el idioma español para Flatpickr ---
+import { Spanish } from "flatpickr/dist/l10n/es.js"; // Importa el locale español
+flatpickr.localize(Spanish); // Establece el idioma español como global por defecto para todas las instancias
