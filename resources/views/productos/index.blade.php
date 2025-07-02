88701b4c19c08ffbdba10c941186ba3b1be5f080
RCandres19 <andresconde45678@gmail.com>
2025-07-01 19:23:00 -0500

    agregando tamaño a la letras de los botones filtrar, exportarCsv y nuevo producto en el index de productos

--- resources/views/productos/index.blade.php
+++ resources/views/productos/index.blade.php
@@ -29,7 +29,7 @@
                 <div class="flex items-center justify-end py-4 space-x-2">
                     <button type="button" id="filterButton"
                         class="inline-flex group items-center justify-center px-4 py-3 space-x-2 space-x-reverse transition-all duration-300 ease-in-out bg-[var(--color-Gestion)] border border-[var(--color-ajustes)] hover:border-[#39A900] rounded-full w-auto">
-                        <span class="text-xs font-medium text-black whitespace-nowrap hover:text-[var(--color-hover)]">
+                        <span class="text-md font-medium text-black whitespace-nowrap hover:text-[var(--color-hover)]">
                             {{ __('Filtrar') }}
                         </span>
                         <img src="{{ asset('images/filtro.svg') }}" class="w-4 h-3 relative inset-0 block group-hover:hidden"
@@ -42,7 +42,7 @@
                         {{-- ! Exportar CSV: Un botón para exportar la lista de productos a un archivo CSV, también con un efecto de cambio de icono al hacer hover. Este botón, aunque es un enlace de navegación, está dentro de un formulario que envía una solicitud GET para la exportación. --}}
                         <x-responsive-nav-link href="#" onclick="this.closest('form').submit(); return false;"
                             class="inline-flex items-center group justify-center px-4 py-3 space-x-2 space-x-reverse transition-all duration-300 ease-in-out bg-[var(--color-Gestion)] border border-[var(--color-ajustes)] hover:border-[#39A900] text-white rounded-full w-auto">
-                            <span class="text-xs font-medium text-black whitespace-nowrap hover:text-[var(--color-hover)]">
+                            <span class="text-md font-medium text-black whitespace-nowrap hover:text-[var(--color-hover)]">
                                 {{ __('Exportar Csv') }}
                             </span>
                             <img src="{{ asset('images/export.svg') }}"
@@ -54,7 +54,7 @@
                     <x-responsive-nav-link href="{{ route('productos.create') }}" {{-- ! Nuevo Usuario: Un botón para crear un nuevo usuario, que tiene un icono y un cambio de color al pasar el ratón. --}}
                         class="inline-flex items-center px-4 py-3 space-x-2 transition-all duration-300 ease-in-out bg-[#39A900] hover:bg-[#61BA33] text-white rounded-full w-auto">
                         <img src="{{ asset('images/signo.svg') }}" class="w-4 h-3" alt="Icono Nuevo Usuario">
-                        <span class="text-xs font-medium whitespace-nowrap">
+                        <span class="text-md font-medium whitespace-nowrap">
                             {{ __('Nuevo Producto') }}
                         </span>
                     </x-responsive-nav-link>
